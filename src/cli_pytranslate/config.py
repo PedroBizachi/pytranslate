1b032383f4ff28ad638186b94689f4ed8717176e
Pedro Bizachi <pedro_bizachi@outlook.com>
2026-08-23 20:34:37 -0300

    feat: Improved TUI interface

--- src/cli_pytranslate/config.py
+++ src/cli_pytranslate/config.py
@@ -1,4 +1,18 @@
-from deep_translator import GoogleTranslator  # pyright: ignore[reportMissingTypeStubs]
+import deep_translator
+from deep_translator import (  # pyright: ignore[reportMissingTypeStubs]
+    BaiduTranslator,
+    ChatGptTranslator,
+    DeeplTranslator,
+    GoogleTranslator,
+    LibreTranslator,
+    LingueeTranslator,
+    MicrosoftTranslator,
+    MyMemoryTranslator,
+    PapagoTranslator,
+    PonsTranslator,
+    QcriTranslator,
+    YandexTranslator,
+)
 from deep_translator.base import (  # pyright: ignore[reportMissingTypeStubs]
     BaseTranslator,
 )
@@ -9,11 +23,61 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
     DEEPL_API: str = Field(default="provide_API_key")
-    DEFAULT_TRANSLATOR: BaseTranslator = GoogleTranslator()
+    default_translator: BaseTranslator = GoogleTranslator()
+    default_translator_name: str = "Google Translator"
     DEFAULT_SOURCE: str = "auto"
     DEFAULT_TARGET: str = "en"
+
+    def get_available_translators(self) -> list[str]:
+        all = deep_translator.__all__
+        result = []
+
+        for item in all:
+            if "Translator" in item:
+                result.append(item)  # pyright: ignore[reportUnknownMemberType]
+
+        return result  # pyright: ignore[reportUnknownVariableType]
+
+    def set_default_translator(self, name: str) -> None:
+        all = deep_translator.__all__
+        if name == all[0]:
+            self.default_translator = GoogleTranslator()
+            self.default_translator_name = "Google Translator"
+        if name == all[1]:
+            self.default_translator = PonsTranslator("en")
+            self.default_translator_name = "Pons Translator"
+        if name == all[2]:
+            self.default_translator = LingueeTranslator()
+            self.default_translator_name = "Linguee Translator"
+        if name == all[3]:
+            self.default_translator = MyMemoryTranslator()
+            self.default_translator_name = "MyMemory Translator"
+        if name == all[4]:
+            self.default_translator = YandexTranslator()
+            self.default_translator_name = "Yandex Translator"
+        if name == all[5]:
+            self.default_translator = MicrosoftTranslator()
+            self.default_translator_name = "Microsoft Translator"
+        if name == all[6]:
+            self.default_translator = QcriTranslator()
+            self.default_translator_name = "Qcri Translator"
+        if name == all[7]:
+            self.default_translator = DeeplTranslator()
+            self.default_translator_name = "Deepl Translator"
+        if name == all[8]:
+            self.default_translator = LibreTranslator()
+            self.default_translator_name = "Libre Translator"
+        if name == all[9]:
+            self.default_translator = PapagoTranslator()
+            self.default_translator_name = "Papago Translator"
+        if name == all[10]:
+            self.default_translator = ChatGptTranslator()
+            self.default_translator_name = "ChatGpt Translator"
+        if name == all[11]:
+            self.default_translator = BaiduTranslator()
+            self.default_translator_name = "Baidu Translator"
 
 
 # Initialize once to share across the application
 settings = Settings()
-translator = settings.DEFAULT_TRANSLATOR
+translator = settings.default_translator
--- src/cli_pytranslate/tui.py
+++ src/cli_pytranslate/tui.py
@@ -1,13 +1,27 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer
+
+from cli_pytranslate.widgets.custom_header import Custom_Header
+from cli_pytranslate.widgets.submit_button import Submit_button
+from cli_pytranslate.widgets.translation_panel import Translation_Panel
 
 
-class PyTranslate(App):
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+class PyTranslate(App):  # pyright: ignore[reportMissingTypeArgument]
+    CSS_PATH = "tui.css"
+
+    BINDINGS = [
+        # TODO: Change ctrl+d to ctrl+c
+        # TODO: ctrl+ENTER to apply translation
+        ("tab", "switch_panels", "Switch translation panels"),
+        # Needs to be dinamic, on settings tab must have "Go to translate" description
+        ("ctrl+s", "switch_tabs", "Go to settings"),
+        ("c", "copy_translated_text", "Copy translated text"),
+    ]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Header()
+        yield Custom_Header()
+        yield Translation_Panel(id="translate-panel")
         yield Footer()
 
     def action_toggle_dark(self) -> None:
