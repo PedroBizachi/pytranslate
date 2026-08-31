7b9872ca2d4ea585faacdbbc3ef7bdd75c6424ff
Pedro Bizachi <pedro_bizachi@outlook.com>
2026-08-31 17:24:16 -0300

    feat: Overall header improvement

--- src/cli_pytranslate/widgets/custom_header.py
+++ src/cli_pytranslate/widgets/custom_header.py
@@ -3,25 +3,25 @@ from typing import cast
 from textual import on
 from textual.app import ComposeResult
 from textual.containers import Container, Horizontal
-from textual.widgets import Select
+from textual.widgets import Label, Select
 
 from cli_pytranslate.config import settings
 from cli_pytranslate.widgets.submit_button import Submit_button
 
 
-class Custom_Header(Container):
+class Custom_Header(Horizontal):
     """A custom header"""
 
     def compose(self) -> ComposeResult:
-        self.engine_list = Select(
+        yield Label("Engine:", id="engine-label")
+        yield Select(
             options=settings.get_available_translators(),
             value=settings.get_available_translators()[0][1],
             id="engine",
-            compact=True,
         )
-        with Horizontal(id="main-row"):
-            yield self.engine_list
-            yield Submit_button(id="submit")
+        with Container(id="app-title-container"):
+            yield Label("PyTranslate", id="app-title")
+        yield Submit_button(id="submit")
 
     @on(Select.Changed)
     def select_changed(self, event: Select.Changed) -> None:
